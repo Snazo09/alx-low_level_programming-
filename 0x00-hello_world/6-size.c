88f7306051b3d99705856390ed32b4e135c60d4f
Snazo09 <snazomgedezi@gmail.com>
2022-12-08 17:25:34 -0800

    6-size.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * mai - Entry Points
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	printf("Size of a char: %lu byte(s)\n", sizeof(char));
+	printf("Size of a int: %lu byte(s)\n", sizeof(char));
+	printf("Size of a long int: %lu byte(s)\n", sizeof(char));
+	printf("Size of a long long int: %lu byte(s)\n", sizeof(char));
+	printf("Size of a float: %lu byte(s)\n", sizeof(char));
+	return (0);
+
+}
