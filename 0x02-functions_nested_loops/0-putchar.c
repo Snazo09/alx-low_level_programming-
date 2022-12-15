77f92df1892f29db57397737433b5bc056a0de46
Snazo09 <snazomgedezi@gmail.com>
2022-12-14 17:45:17 -0800

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,20 +1,23 @@
+#include <stdio.h>
 #include "main.h"
-/**
- * main - print _putchar
- * is written by mbah
- * Return: 0
- */
 
+/**
+ * main - Entry point
+ * 
+ *Description: A program that prints _putchar and new line
+ *
+ * Return: returns 0
+ **/
 int main(void)
 {
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
+	char c[8] = "_putchar";
+	int i = 0;
+	
+	while (i < 8)
+	{
+		_putchar(c[i]);
+		i++;
+	}
 	_putchar('\n');
 	return (0);
 }
--- 0x02-functions_nested_loops/_putchar.c
+++ 0x02-functions_nested_loops/_putchar.c
@@ -4,8 +4,8 @@
  * _putchar - writes the character c to stdout
  * @c: The character to print
  *
- * Return: 0n Success 1.
- * 0n error, -1 is returned,and errno is set appropriately.
+ * Return: 0n success 1.
+ * 0n error, -1 is returned, and errno is set appropriately.
  */
 int _putchar(char c)
 {
