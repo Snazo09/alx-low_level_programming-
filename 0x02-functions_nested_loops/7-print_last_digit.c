3ca7643c7b42b89bc7509c735ad6d0b73eb22837
Snazo09 <119118651+Snazo09@users.noreply.github.com>
2022-12-15 22:23:12 +0200

    Update 7-print_last_digit.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,10 +1,7 @@
 #!/usr/bin/python3
 def print_last_digit(number):
-    if number >= 0:
-        l_digit = number % 10
-    else:
-        l_digit = number % -10
-        l_digit *= -1
-
-    print("{:d}".format(l_digit), end='')
-    return (l_digit)
+    digit = number % 10
+    if number < 0:
+        digit = (number * -1) % 10
+    print("{:d}".format(digit), end="")
+    return digit
