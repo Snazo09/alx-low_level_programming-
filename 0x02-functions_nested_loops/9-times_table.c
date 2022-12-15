ca5b57e31039de7a9397ee8a5e5bf7f009ae6d22
Snazo09 <snazomgedezi@gmail.com>
2022-12-15 11:15:44 -0800

    9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -21,7 +21,7 @@ void times_table(void)
 				}
 
 				if (b != 0)
-				{
+
 					_putchar(' ');
 					_putchar((c % 10) + '0');
 				}
@@ -29,15 +29,13 @@ void times_table(void)
 				{
 					_putchar(',');
 					_putchar(' ');
-				}
 			}
 			else
 			{
 				_putchar((c / 10) + '0');
 				_putchar((c % 10) + '0');
+			}
 				if (b < 9)
 			}
 		}
 		_putchar('\n');
-	}
-}	
