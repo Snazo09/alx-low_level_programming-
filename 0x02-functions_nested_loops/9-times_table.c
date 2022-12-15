a3e5e2982ce590ab53427a1e499ab5ecf87bb5bb
Snazo09 <119118651+Snazo09@users.noreply.github.com>
2022-12-15 22:49:34 +0200

    Update 9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -21,7 +21,7 @@ void times_table(void)
 				}
 
 				if (b != 0)
-
+				{
 					_putchar(' ');
 					_putchar((c % 10) + '0');
 				}
@@ -29,14 +29,15 @@ void times_table(void)
 				{
 					_putchar(',');
 					_putchar(' ');
+				}
 			}
 			else
 			{
 				_putchar((c / 10) + '0');
 				_putchar((c % 10) + '0');
-			}
 				if (b < 9)
 			}
 		}
-
 		_putchar('\n');
+	}
+}
