1bb3e0993759d81ef169a976597806f1c4a8267d
Snazo09 <119118651+Snazo09@users.noreply.github.com>
2023-01-12 22:07:54 +0200

    Update 1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,48 +1,52 @@
+#include "main.h"
 #include <stdlib.h>
-#include <stdio.h>
-#include "holberton.h"
 
 /**
- * string_nconcat - concatenates two strings
- *
- * @s1: first string
- * @s2: second string
- * @n: how many bytes of second string to concatenate
- * Return: pointer to concatenated string
- */
+  * string_nconcat - ...
+  * @s1: ...
+  * @s2: ...
+  * @n: ...
+  *
+  * Return: ...
+  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	char *c;
-	unsigned int i, k, a;
+	unsigned int i = 0, j = 0, k = 0, l = 0;
+	char *str;
 
 	if (s1 == NULL)
-	{
-		i = 0;
-	}
-	else
-	{
-		for (i = 0; *(s1 + i) != '\0'; i++)
-			;
-	}
+		s1 = "";
 	if (s2 == NULL)
-	{
-		k = 0;
-	}
+		s2 = "";
+
+	while (s1[i])
+		i++;
+
+	while (s2[k])
+		k++;
+
+	if (n >= k)
+		l = i + k;
 	else
-	{
-		for (k = 0; *(s2 + k) != '\0'; k++)
-			;
-		if (n < k)
-			k = n;
-	}
-	k += i;
-	c = malloc(sizeof(char) * (k + 1));
-	if (c == NULL)
+		l = i + n;
+
+	str = malloc(sizeof(char) * l + 1);
+	if (str == NULL)
 		return (NULL);
-	for (a = 0; a < i; a++)
-		c[a] = *(s1 + a);
-	for (i = 0; a < k; i++)
-		c[a++] = *(s2 + i);
-	c[a] = '\0';
-	return (c);
+
+	k = 0;
+	while (j < l)
+	{
+		if (j <= i)
+			str[j] = s1[j];
+
+		if (j >= i)
+		{
+			str[j] = s2[k];
+			k++;
+		}
+		j++;
+	}
+	str[j] = '\0';
+	return (str);
 }
