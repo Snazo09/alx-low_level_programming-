07d0ee3c3f0b736c18bcfd3fd96d293f63ad31ad
Snazo09 <119118651+Snazo09@users.noreply.github.com>
2023-01-19 17:16:10 +0200

    Update 3-op_functions.c

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -1,121 +1,73 @@
 #include "3-calc.h"
+#include <stddef.h>
+#include <stdlib.h>
+#include <stdio.h>
 
-        int op_add(int a, int b);
+/**
+ * op_add - adds two numbers.
+ * @a: first number.
+ * @b: second number.
+ *
+ * Return: add.
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
 
-        int op_sub(int a, int b);
+/**
+ * op_sub - subctracts two numbers.
+ * @a: first number.
+ * @b: second number.
+ *
+ * Return: difference.
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
 
-        int op_mul(int a, int b);
+/**
+ * op_mul - multiplies two numbers.
+ * @a: first number.
+ * @b: second number.
+ *
+ * Return: multiplication.
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
 
-        int op_div(int a, int b);
+/**
+ * op_div - divides two numbers.
+ * @a: first number.
+ * @b: second number.
+ *
+ * Return: division.
+ */
+int op_div(int a, int b)
+{
+	if (b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+		return (a / b);
+}
 
-        int op_mod(int a, int b);
-
-        /**
-
-         * op_add - Returns the sum of two numbers.
-
-         * @a: The first number.
-
-         * @b: The second number.
-
-         *
-
-         * Return: The sum of a and b.
-
-         */
-
-        int op_add(int a, int b)
-
-        {
-
-                return (a + b);
-
-        }
-
-        /**
-
-         * op_sub - Returns the difference of two numbers.
-
-         * @a: The first number.
-
-         * @b: The second number.
-
-         *
-
-         * Return: The difference of a and b.
-
-         */
-
-        int op_sub(int a, int b)
-
-        {
-
-                return (a - b);
-
-        }
-
-        /**
-
-         * op_mul - Returns the product of two numbers.
-
-         * @a: The first number.
-
-         * @b: The second number.
-
-         *
-
-         * Return: The product of a and b.
-
-         */
-
-        int op_mul(int a, int b)
-
-        {
-
-                return (a * b);
-
-        }
-
-        /**
-
-         * op_div - Returns the division of two numbers.
-
-         * @a: The first number.
-
-         * @b: The second number.
-
-         *
-
-         * Return: The quotient of a and b.
-
-         */
-
-        int op_div(int a, int b)
-
-        {
-
-                return (a / b);
-
-        }
-
-        /**
-
-         * op_mod - Returns the remainder of the division of two numbers.
-
-         * @a: The first number.
-
-         * @b: The second number.
-
-         *
-
-         * Return: The remainder of the division of a by b.
-
-         */
-
-        int op_mod(int a, int b)
-
-        {
-
-                return (a % b);
-
-        }
+/**
+ * op_mod - calculates the module of two numbers.
+ * @a: first number.
+ * @b: second number.
+ *
+ * Return: remainder of the division.
+ */
+int op_mod(int a, int b)
+{
+	if (b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	return (a % b);
+}
