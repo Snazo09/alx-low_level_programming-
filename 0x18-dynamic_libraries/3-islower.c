bc1299a1fa64fced44184ad05c3078967d3e9724
Snazo09 <snazomgedezi@gmail.com>
2023-03-20 11:56:37 -0700

    0x18. C - Dynamic libraries

---
+++ 0x18-dynamic_libraries/3-islower.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * _islower -> checks if the character is lowercase
+ * @c: a character argumet
+ * Return: returns 1 and 0 depending on condition
+ */
+int _islower(int c)
+{
+	return (c >= 'a' && c <= 'z');
+}
---
+++ 0x18-dynamic_libraries/4-isalpha.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * _isalpha - chcks for alphabetical letters
+ * @c: a character to be checked on
+ * Return: returns 0 or 1 depending on condition
+ */
+int _isalpha(int c)
+{
+	return ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'));
+}
